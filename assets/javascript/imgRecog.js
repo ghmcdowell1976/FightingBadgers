e4334e597574982c746e1bf92b6f658096c1e06e
alevens1 <alevesn1@icloud.com>
2017-07-13 18:36:46 -0400

    removed table template

--- assets/javascript/imgRecog.js
+++ assets/javascript/imgRecog.js
@@ -1,47 +1,6 @@
 "use strict"
 $(document).ready(function(){
 
-	/* hard code some of the variables to be used in the call */
-
-	/* calls to Face++ */
-	var request_url = "https://api-us.faceplusplus.com/facepp/v3/face/analyze";
-	var detects_url = "https://api-us.faceplusplus.com/facepp/v3/detect";
-
-	/* necessary keys */
-	var myKey = "M-I560kGVe7hslv83g_CTxfVNU1qtO3u";
-	var mySec = "LZ7qc7rfLRPKuS1DxYrEeoi1jBQUcWD2";
-
-
-	/* sample images from the web */
-	/* -------------------------- */
-
-	/* smiling man to be used in the trial */
-	/* ----------------------------------- */
-	var hMan = "http://cdn.acidcow.com/pics/20110408/smiles_24.jpg";
-
-	/* frowning prez */
-	var sMan = "http://drvidyahattangadi.com/wp-content/uploads/2014/07/A110.jpg";
-
-	/* return attributes string */
-	var faceAtts = "gender,age,smiling,headpose,facequality,blur,eyestatus,ethnicity,emotion";
-
-
-	/* test the Face++ API key */
-	$.ajax({
-
-url: detects_url + "?api_key="+ myKey + "&api_secret="+ mySec + "&image_url="+ hMan + "&return_landmark=1" + "&return_attributes=" + faceAtts,	/* image URL */
-method: "POST"			/* is the information coming/going? */
-
-	}).done(function(response){
-
-		/* console log out the response */
-		console.log(response);
-		console.log(response.faces);
-		console.log(response.faces[0].attributes);
-		console.log(response.faces[0].attributes['emotion']);
-	});
-
-
 	var imgSrc = function(){
 
 		/* these control the internal camera's functionality */
@@ -49,7 +8,6 @@ method: "POST"			/* is the information coming/going? */
 		function on(){
 			/* turns on the camera */
 			console.log("camera is on");
-
 		};
 
 		function capture(){
@@ -64,8 +22,54 @@ method: "POST"			/* is the information coming/going? */
 
 		function off(){
 			/* turns camera off */
+			console.log("camera is off");
 		};
 
+
+		/* once the image is captured, it gets analyzed here */
+		/* ------------------------------------------------- */
+
+		/* calls to Face++ */
+		/* here are the URLs to request information from F++ */
+		/* ------------------------------------------------- */
+		var request_url = "https://api-us.faceplusplus.com/facepp/v3/face/analyze";
+		var detects_url = "https://api-us.faceplusplus.com/facepp/v3/detect";
+
+		/* necessary keys */
+		var myKey = "M-I560kGVe7hslv83g_CTxfVNU1qtO3u";		/* api key */
+		var mySec = "LZ7qc7rfLRPKuS1DxYrEeoi1jBQUcWD2";		/* api secret */
+
+
+		/* sample images from the web */
+		/* -------------------------- */
+
+		/* smiling man to be used in the trial */
+		/* ----------------------------------- */
+		var hMan = "http://cdn.acidcow.com/pics/20110408/smiles_24.jpg";
+
+		/* frowning prez */
+		var sMan = "http://drvidyahattangadi.com/wp-content/uploads/2014/07/A110.jpg";
+
+		/* return attributes string */
+		/* this is the information we want Face++ will return about the faces it sees */
+		/* -------------------------------------------------------------------------- */
+		var faceAtts = "gender,age,smiling,headpose,facequality,blur,eyestatus,ethnicity,emotion";
+
+		$.ajax({
+
+url: detects_url + "?api_key="+ myKey + "&api_secret="+ mySec + "&image_url="+ hMan + "&return_landmark=1" + "&return_attributes=" + faceAtts,	/* image URL */
+method: "POST"			/* is the information coming/going? */
+
+		}).done(function(response){
+
+			/* console log out the response */
+			console.log(response);
+			console.log(response.faces);
+			console.log(response.faces[0].attributes);
+			console.log(response.faces[0].attributes['emotion']);
+
+			// $("#selfie-camera").html("<img src=" + hMan + ">");
+		});
 
 
 		return {
@@ -76,5 +80,8 @@ method: "POST"			/* is the information coming/going? */
 		}
 
 	}();
+
+imgSrc.wakeCamera();
+imgSrc.useCamera();
 
 });
